4c90848d814d07deb3c3fcef1eb1847662997489
Sergey <saizotov1@gmail.com>
2023-03-31 15:17:15 +0500

    Решение задачи номер 12

---
+++ Задача012/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число ");
+int nummer = int.Parse(Console.ReadLine());
+int length = nummer;
+if (length >= 100)
+{
+    while (nummer>999)
+    {
+      nummer = nummer/10;
+    }
+      int result = nummer % 10;  
+
+    Console.WriteLine($"Третье число - {result}");
+}
+else
+{
+    Console.WriteLine($"Нет третьего числа");
+}
